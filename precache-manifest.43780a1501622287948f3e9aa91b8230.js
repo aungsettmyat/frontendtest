4fc9834157bc5643cb679b3af0d14b94cc6f0129
AungSettMyat <48052796+aungsettmyat@users.noreply.github.com>
2019-04-08 15:53:53 +0900

    Updates

---
+++ precache-manifest.43780a1501622287948f3e9aa91b8230.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bda927e9dbb0f09851e4",
+    "url": "/frontendtest/static/js/runtime~main.bda927e9.js"
+  },
+  {
+    "revision": "e3c4e33a7fae9ff43bdf",
+    "url": "/frontendtest/static/js/main.e3c4e33a.chunk.js"
+  },
+  {
+    "revision": "60ec5202fc8675e9b488",
+    "url": "/frontendtest/static/js/1.60ec5202.chunk.js"
+  },
+  {
+    "revision": "e3c4e33a7fae9ff43bdf",
+    "url": "/frontendtest/static/css/main.72c4040d.chunk.css"
+  },
+  {
+    "revision": "7af15747beb1bf2c82acc2f59c374c36",
+    "url": "/frontendtest/index.html"
+  }
+];
